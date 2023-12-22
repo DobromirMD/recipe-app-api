7ba8861305831f34f8f77f3864477e578f4455ec
DobromirMD <dannchev32@gmail.com>
2023-12-22 23:17:16 +0200

    Added admin functionality

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,6 +16,7 @@ class Command(BaseCommand):
         """Entry point for command."""
         self.stdout.write('Waiting for database..')
         db_up = False
+
         while db_up is False:
             try:
                 self.check(databases=['default'])
